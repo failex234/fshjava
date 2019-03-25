43f28e8b70fa05ddc171180d16b4f1625b58e874
Felix Naumann <felix@emilnaumann.de>
2019-03-25 10:22:59 +0100

    fixed error detection when trying to cd to a file or a non-existing file

--- fshutils.c
+++ fshutils.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/stat.h>
 #include "fshutils.h"
 
 #ifdef __WIN32
@@ -13,6 +14,25 @@
 #include <sys/types.h>
 
 #endif
+
+int isDirectory(const char *path) {
+    struct stat filestats;
+    if (stat(path, &filestats) != 0) {
+        return 0;
+    }
+    return S_ISDIR(filestats.st_mode);
+}
+
+int fileExists(const char *path) {
+    FILE *file = fopen(path, "r");
+
+    if (file == NULL) {
+        return 0;
+    } else {
+        fclose(file);
+        return 1;
+    }
+}
 
 JNIEXPORT jboolean JNICALL Java_me_felixnaumann_reflection_Utils_Native_isAdministrator(JNIEnv *env, jclass clazz) {
 #ifdef __WIN32
@@ -41,6 +61,15 @@ JNIEXPORT jboolean JNICALL Java_me_felixnaumann_reflection_Utils_Native_isAdmini
 JNIEXPORT jint JNICALL Java_me_felixnaumann_reflection_Utils_Native_changeWorkingDirectory(JNIEnv *env, jclass clazz, jstring dest) {
 	char *destination = (*env)->GetStringUTFChars(env, dest, 0);
 
+    if (destination[0] != '.') {
+	    if (!isDirectory(destination)) {
+	        return -3;
+    	} else if (!fileExists(destination) && !isDirectory(destination)) {
+	        return -2;
+	    }
+	}
+
+    errno = 0;
 	chdir(destination);
 	
 	if (errno == EACCES) {
@@ -57,7 +86,8 @@ JNIEXPORT jint JNICALL Java_me_felixnaumann_reflection_Utils_Native_changeWorkin
 
 JNIEXPORT jstring JNICALL Java_me_felixnaumann_reflection_Utils_Native_getWorkingDirectory(JNIEnv *env, jclass clazz) {
 	char *buf = malloc(sizeof(char) * 512);
-	
+
+	errno = 0;
 	if (getcwd(buf, 512) == NULL) {
 		if (errno == EACCES) {
 			strcpy(buf, "Access denied!");
--- src/me/felixnaumann/reflection/Utils/FileUtils.java
+++ src/me/felixnaumann/reflection/Utils/FileUtils.java
@@ -5,15 +5,14 @@ import me.felixnaumann.reflection.Debug.DebuggingTools;
 public class FileUtils {
 
     public static void setWorkingDirectory(String wd) {
+        String currdir = Native.getWorkingDirectory();
         int result = Native.changeWorkingDirectory(wd);
         String newdir = Native.getWorkingDirectory();
 
-        if (result == -1 && wd.equals(newdir)) {
+        if (result == -1 && currdir.equals(newdir)) {
             System.err.println("fsh: access denied");
-        } else if (result == -2 && wd.equals(newdir)) {
+        } else if ((result == -2 || result == -3) && currdir.equals(newdir)) {
             System.err.println("fsh: no such file or directory");
-        } else if (result == -3 && wd.equals(newdir)) {
-            System.err.println("fsh: this is not a directory");
         } else {
             DebuggingTools.logf("Changed to %s\n", wd);
             System.setProperty("user.dir", Native.getWorkingDirectory());
